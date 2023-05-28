f45771205e94a8d6c89c3459a287ea1f1a11190b
jasonbanboa <jasonbanboa123@gmail.com>
2023-05-28 17:42:37 +0900

    added util function to get data and set mock data

--- js/common.js
+++ js/common.js
@@ -1,71 +1,83 @@
 import { 
-  generateUniqueID,
   $, 
   $$,
+  generateUniqueID,
+  getKanbanData,
+  mockData,
 } from '../lib/util.js' 
 
-console.log(generateUniqueID());
 
-const $sectionContainers = $$('.section-container');
-const $sections = $$('.section');
+// localStorage.setItem('kanban', JSON.stringify(mockData));
 
-$sections.forEach(($section) => {
-  $section.addEventListener('dragstart', (e) => {
-    e.stopPropagation();
-    $section.classList.add('is-dragging');
+const kanbanData = getKanbanData();
+console.log(kanbanData);
+
+function main() {
+  // these eventListeners need to be updated when new sections or todos are added;
+  
+  // section eventListeners
+  const $sectionContainers = $$('.section-container');
+  const $sections = $$('.section');
+  
+  $sections.forEach(($section) => {
+    $section.addEventListener('dragstart', (e) => {
+      e.stopPropagation();
+      $section.classList.add('is-dragging');
+    });
+  
+    $section.addEventListener('dragend', (e) => {
+      e.stopPropagation();
+      $section.classList.remove('is-dragging');
+    });
   });
-
-  $section.addEventListener('dragend', (e) => {
-    e.stopPropagation();
-    $section.classList.remove('is-dragging');
+  
+  $sectionContainers.forEach(($sectionContainer) => {
+    $sectionContainer.addEventListener('dragover', (e) => {
+      e.preventDefault();
+  
+      const $currentSection = $('.is-dragging:not(.todo)');
+      if (!$currentSection) return;
+  
+      const $currentSectionContainer = $currentSection.closest('.section-container');
+      const $sectionToBeSwitched = $sectionContainer.querySelector('.section');
+  
+      $currentSectionContainer.append($sectionToBeSwitched);
+      $sectionContainer.append($currentSection);
+    });
+  }); 
+  
+  // todo eventListeners
+  const $todos = $$('.todo');
+  const $sectionsBodies = $$('.section-body');
+  
+  $todos.forEach(($todo) => {
+    $todo.addEventListener('dragstart', (e) => {
+      e.stopPropagation();
+      $todo.classList.add('is-dragging');
+    });
+    $todo.addEventListener('dragend', (e) => {
+      e.stopPropagation();
+      $todo.classList.remove('is-dragging');
+    });
   });
-});
-
-$sectionContainers.forEach(($sectionContainer) => {
-  $sectionContainer.addEventListener('dragover', (e) => {
-    e.preventDefault();
-
-    const $currentSection = $('.is-dragging:not(.todo)');
-    if (!$currentSection) return;
-
-    const $currentSectionContainer = $currentSection.closest('.section-container');
-    const $sectionToBeSwitched = $sectionContainer.querySelector('.section');
-
-    $currentSectionContainer.append($sectionToBeSwitched);
-    $sectionContainer.append($currentSection);
+  
+  $sectionsBodies.forEach($section => {
+    $section.addEventListener('dragover', (e) => {
+      e.preventDefault(); 
+      
+      const $currentTodo = $('.is-dragging:not(.section)');
+      
+      if (!$currentTodo) return;
+      
+      const $bottomTodo = getDragAfterTodo($section, e.clientY);
+      if ($bottomTodo) {
+        $section.insertBefore($currentTodo, $bottomTodo);
+      } else {
+        $section.append($currentTodo);
+      }
+    });
   });
-}); 
-
-const $todos = $$('.todo');
-const $sectionsBodies = $$('.section-body');
-
-$todos.forEach(($todo) => {
-  $todo.addEventListener('dragstart', (e) => {
-    e.stopPropagation();
-    $todo.classList.add('is-dragging');
-  });
-  $todo.addEventListener('dragend', (e) => {
-    e.stopPropagation();
-    $todo.classList.remove('is-dragging');
-  });
-});
-
-$sectionsBodies.forEach($section => {
-  $section.addEventListener('dragover', (e) => {
-    e.preventDefault(); 
-    
-    const $currentTodo = $('.is-dragging:not(.section)');
-    
-    if (!$currentTodo) return;
-    
-    const $bottomTodo = getDragAfterTodo($section, e.clientY);
-    if ($bottomTodo) {
-      $section.insertBefore($currentTodo, $bottomTodo);
-    } else {
-      $section.append($currentTodo);
-    }
-  });
-});
+}
 
 function getDragAfterTodo($section, mouseY) {
   const $todos = [...$section.querySelectorAll('.todo:not(.is-dragging)')];
@@ -82,6 +94,7 @@ function getDragAfterTodo($section, mouseY) {
   }, { offset: Number.NEGATIVE_INFINITY }).element;
 }
 
-function getDragAfterSection() {
-  
-}
+
+window.onload = () => {
+  main();
+} 
